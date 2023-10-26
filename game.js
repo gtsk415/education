02473c2d6e006d176f365001b8a0da178cb2f304
gtsk415 <print.bistro@mail.ru>
2023-10-26 21:23:31 +0300

    game

---
+++ game.js
@@ -0,0 +1,24 @@
+let gameRuleset = {
+  diceNumber: 2,
+  maxAttempts: 3
+};
+
+let firstCat = {
+  name: 'Кекс',
+  points: 0
+};
+
+let secondCat = {
+  name: 'Рудольф',
+  points: 0
+};
+
+let cats = [firstCat, secondCat];
+
+
+// rulest === gameRuleset, plaers === [cats]
+let runGame = function (rulest, palyers) { 
+  for ( let currentAttempt = 1; currentAttempt <= gameRuleset <= ruleset.maxAttemps; currentAttempt++) {
+    
+  }
+};
